84d47028e6f01a4be13d8ef3d4148bdf39d94a03
Hanne Olsen <hanne.olsen@nav.no>
2025-08-13 16:14:41 +0200

    Endrer tekst slik at beskrivelsen forhåpentligvis skal være på en linje (#59)

--- src/language/useTranslate.tsx
+++ src/language/useTranslate.tsx
@@ -12,9 +12,9 @@ const text: Text = {
     en: "Child benefit",
   },
   microfrontendDescription: {
-    nb: "Se dokumenter og meld fra om endringer.",
-    nn: "Sjå dokument og meld frå om endringar.",
-    en: "View documents and notify Nav of changes.",
+    nb: "Se dokumenter og meld endringer",
+    nn: "Sjå dokument og meld endringar",
+    en: "View documents and notify of changes",
   },
 };
 
