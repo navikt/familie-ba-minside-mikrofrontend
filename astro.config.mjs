eb5c68733da45a802dda0e2bbb426a9b3b243abe
Nguyen <nghi.nguyen@nav.no>
2025-10-23 12:20:50 +0200

    testing workflow updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,10 +53,10 @@ export default defineConfig({
   }),
   env: {
     schema: {
-      EXAMPLE_API_URL: envField.string({
+      URL_BARNETRYGD_MIN_SIDE: envField.string({
         context: "server",
         access: "secret",
-        default: "http://localhost:3000/api/tms-astro-template",
+        default: "http://localhost:3000/api/tms-astro-template", //TODO change default value
       }),
     },
   },
--- src/utils/url.tsx
+++
@@ -1,8 +0,0 @@
-const urlIMiljø = {
-  prod: "https://www.nav.no/barnetrygd/min-barnetrygd",
-  dev: "https://familie-ba-minside.intern.dev.nav.no/barnetrygd/min-barnetrygd",
-};
-export const getUrl = (url: URL) => {
-  const erDev = url.href.includes("www.ansatt.dev.nav.no") || url.href.includes("www.intern.dev.nav.no");
-  return erDev ? urlIMiljø.dev : urlIMiljø.prod;
-};
