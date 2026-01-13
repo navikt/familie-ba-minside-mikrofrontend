671b0c99e5c349faca8dbdb893cf54e1325a6951
thoalm <thoalm@outlook.com>
2026-01-13 15:34:46 +0100

    NAV-26563: Legger til .prettierrc

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
-import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import jsxA11y from "eslint-plugin-jsx-a11y";
-import react from "eslint-plugin-react";
-import tseslint from "typescript-eslint";
-import eslintConfigPrittier from "eslint-config-prettier";
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrittier from 'eslint-config-prettier';
 
 export default defineConfig([
-  eslint.configs.recommended,
-  eslintConfigPrittier,
-  tseslint.configs.recommended,
-  react.configs.flat.recommended,
-  react.configs.flat["jsx-runtime"],
-  jsxA11y.flatConfigs.recommended,
-  {
-    ignores: ["dist/**/*", "src/env.d.ts"],
-  },
-  {
-    settings: {
-      react: {
-        version: "detect",
-      },
+    eslint.configs.recommended,
+    eslintConfigPrittier,
+    tseslint.configs.recommended,
+    react.configs.flat.recommended,
+    react.configs.flat['jsx-runtime'],
+    jsxA11y.flatConfigs.recommended,
+    {
+        ignores: ['dist/**/*', 'src/env.d.ts'],
     },
-  },
+    {
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
 ]);
