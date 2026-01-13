c2284f00683206b8ea4a46d564ab5f0f4b57c2b1
thoalm <thoalm@outlook.com>
2026-01-13 14:56:13 +0100

    NAV-26563: Sikkerhetsoppdateringer

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@ import { defineConfig } from "eslint/config";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
+import eslintConfigPrittier from "eslint-config-prettier";
 
 export default defineConfig([
   eslint.configs.recommended,
+  eslintConfigPrittier,
   tseslint.configs.recommended,
   react.configs.flat.recommended,
   react.configs.flat["jsx-runtime"],
